d3ed113b52b67c1c8015dffdeb1862b54b621568
Evan McGrane <mcgrane480@gmail.com>
2022-01-04 22:44:39 +0000

    fix1

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use("/api/private", require("./routes/private"));
 app.use("/api/users", require("./routes/users"))
 // app.use("/", require("./routes/users"))
 app.use(errorHandler);
-app.use(cors())
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions)) // Use this after the variable declaration
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => console.log(`serving on port ${PORT}`))
